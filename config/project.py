73fae4634b1f0cdd4819227fe9b399734c297023
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-27 00:33:59 +0300

    better project config

--- config/project.py
+++ config/project.py
@@ -23,7 +23,7 @@ project_keywords = [
     "convert to text",
 ]
 project_license = "MIT"
-project_year_started = "2020"
+project_year_started = 2020
 project_description = project_short_description
 project_platforms = [
     "python3",
