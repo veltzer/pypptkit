0af6e29df35f8b36198506e86ace8ee2e773809b
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 02:53:35 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,24 +1,29 @@
-console_scripts = [
+from typing import List
+
+
+console_scripts: List[str] = [
     "pypptkit=pypptkit.main:main",
 ]
-dev_requires = [
+config_requires: List[str] = []
+dev_requires: List[str] = [
     "pypitools",
 ]
-make_requires = [
-    "pymakehelper",
-    "pyclassifiers",
-    "pydmt",
-]
-install_requires = [
+install_requires: List[str] = [
     "pytconf",
     "pylogconf",
     "python-pptx",
     "pyvardump",
 ]
-test_requires = [
+make_requires: List[str] = [
+    "pymakehelper",
+    "pyclassifiers",
+    "pydmt",
+]
+test_requires: List[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
     "flake8",
     "mypy",
 ]
+requires = config_requires + install_requires + make_requires + test_requires
